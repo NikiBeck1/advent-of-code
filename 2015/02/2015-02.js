17a54ecf9d40758ac600798a1fa89728437c9f90
Niki Beck <niki@wearehamburger.com>
2025-03-25 15:38:15 -0500

    Refactored initial grouping with Billy and Tyler

--- 2015/02/2015-02.js
+++ 2015/02/2015-02.js
@@ -3,14 +3,12 @@ var data = fs.readFileSync('2015/02/2015-02.data').toString()
 
 function floors(square) {
   // Grouping the values
-  let measurements = square.toString()
-  let nums = measurements.match(/\d+/g).map(Number)
-  let grouped = []
-  for (let i = 0; i < nums.length; i += 3) {
-    if (nums.slice(i, i + 3).length === 3) {
-      grouped.push(nums.slice(i, i + 3))
-    }
-  }
+  let grouped = square
+    .trim()
+    .split('\n')
+    .map((singleGroup) => singleGroup.split('x').map(Number))
+
+  console.log(grouped)
 
   let smallestValues = grouped
     .map((singleGroup) => {
