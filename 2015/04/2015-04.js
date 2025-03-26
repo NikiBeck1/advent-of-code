c3db18d8e081aced46f53cf09883e886886b4c75
Niki Beck <niki@wearehamburger.com>
2025-03-26 16:11:00 -0500

    Solved exercise 4

--- 2015/04/2015-04.js
+++ 2015/04/2015-04.js
@@ -1,6 +1,24 @@
 var fs = require('fs')
-var data = fs.readFileSync('2015/04/2015-04.data').toString()
+const crypto = require('crypto')
 
-function floors(x) {}
+function md5(str) {
+  return crypto.createHash('md5').update(str).digest('hex')
+}
 
-console.log(floors(data))
+function floors() {
+  let index = 0
+  let secretCode = 'ckczppom'
+
+  while (true) {
+    let attempt = secretCode + index
+    let hash = md5(attempt)
+
+    if (hash.startsWith('00000')) {
+      return index
+    }
+
+    index++
+  }
+}
+
+console.log(floors())
